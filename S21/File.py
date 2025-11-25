6a3d7bf2ab75247db5fe0ae7c7ec595c92eb7adf
Malihe Hajihosseini <m.hajihosseini95@gmail.com>
2025-11-25 08:15:07 +0330

    S21

---
+++ S21/File.py
@@ -0,0 +1,44 @@
+import random
+
+def Save_Int_File(filename, n, a, b):
+    f = open(filename, 'w')
+
+    for i in range(n):
+        num = random.randint(a,b)
+        f.write(str(num)+"\n")
+    
+    f.close()
+
+
+#Save_Int_File("numbers.txt", 50, 1, 20)
+#print(random.randint(10,20))
+
+def Get_Index(arr, value):
+    for i in range(len(arr)):
+        if arr[i] == value:
+            return i
+
+
+def Count_Nums_File(filename):
+    f = open(filename, 'r')
+    lines = f.readlines()
+
+    UniqueNumbers = []
+    CountNums = []
+
+    for li in lines:
+        li = int(li.strip())
+        if li in UniqueNumbers:
+            index = Get_Index(UniqueNumbers, li)
+            CountNums[index] += 1
+        
+        else:
+            UniqueNumbers.append(li)
+            CountNums.append(1)
+
+    
+    for i in range(len(UniqueNumbers)):
+        print(f"{UniqueNumbers[i]}:{CountNums[i]}")
+
+
+Count_Nums_File("numbers.txt")
