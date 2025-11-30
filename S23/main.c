a9bda66ebb82a13de3bd6efbaf67b54e917c092c
Sauleh Eetemadi <sauleh@gmail.com>
2025-11-30 11:03:16 +0400

    u

--- S23/main.c
+++ S23/main.c
@@ -1,21 +1,60 @@
 #include<stdio.h>
 
+
+unsigned int get_bit(unsigned int n, int i)
+{
+    return (n >> i) % 2;
+}
+
+unsigned int set_bit(unsigned int n, int i)
+{
+    
+}
+
+unsigned int clear_bit(unsigned int n, int i)
+{
+
+}
+
+unsigned int reverse_bits(unsigned int n)
+{
+    
+}
+
+
 unsigned int power2(int n)
 {
-    // ...
-    //while
+    int i = 0;
+    unsigned int result = 1;
+    while (i<n)
+    {
+        result = result * 2;
+        i++;
+    }
+    return result;
+}
+
+int get_last_bit(unsigned int n)
+{
+    int bits = sizeof(n) * 8;    
+    unsigned int lastbitOn = power2(bits-1); // 1 << bits-1;
+    unsigned int b = n & lastbitOn;
+    if (b != 0)
+        return 1;
+    else
+        return 0;
 }
 
 void print_binary_proper(unsigned int n)
 {
-    int bits = sizeof(n) * 4;
-    unsigned int lastbitOn = power2(bits);
-    int b = n & lastbitOn;
-    if (b > 0)
-        printf("1");
-    else
-        printf("0");
-    n = n * 2;
+    int bits = sizeof(n) * 8;    
+    int i = 0;
+    while (i < bits)
+    {
+        printf("%d", get_last_bit(n));
+        n = n * 2; // n << 1;
+        i++;
+    }
 }
 
 void print_binary(int n)
@@ -25,7 +64,7 @@ void print_binary(int n)
     {
         int b = n % 2;
         printf("%d ", b);
-        n = n / 2;
+        n = n / 2;  // n >> 1
         i = i + 1;
     }
 }
@@ -33,11 +72,14 @@ void print_binary(int n)
 
 int main(int argc, char** argv)
 {
-    int n;
-    while (1)
-    {
-        scanf("%d", &n);
-        print_binary(n);
-    }
+    unsigned char n = 200;
+    printf("%d\n", n);
+
+    // int n;
+    // // while (1)
+    // {
+    //     // scanf("%d\n", &n);
+    //     print_binary_proper(43);
+    // }
     return 0;
 }
---
+++ S23/main2.c
@@ -0,0 +1,12 @@
+
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+
+void main()
+{
+    int n = add(5, 4);
+}
